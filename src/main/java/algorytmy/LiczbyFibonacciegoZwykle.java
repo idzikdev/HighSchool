adf9da628d52ba7efed5e16cd6ec81032b9b6c12
Tomasz Idzikowski <idzikdev@gmail.com>
2018-11-22 15:46:33 +0100

    initial commit

---
+++ src/main/java/algorytmy/LiczbyFibonacciegoZwykle.java
@@ -0,0 +1,22 @@
+package algorytmy;
+
+import java.util.Scanner;
+
+public class LiczbyFibonacciegoZwykle {
+    public static void main(String[] args) {
+        int a=1;
+        int b=1;
+        int suma;
+        Scanner scanner=new Scanner(System.in);
+        System.out.println();
+        int ile=scanner.nextInt();
+        System.out.println("1");
+        System.out.println("1");
+        for (int i = 1; i <=ile-2 ; i++) {
+            suma=a+b;
+            a=b;
+            b=suma;
+            System.out.println(suma);
+        }
+    }
+}
