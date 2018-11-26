b8e9808573d04391b6b49b4373fd174128ad32d9
Tomasz Idzikowski <idzikdev@gmail.com>
2018-11-26 10:23:41 +0100

    26.11.2018

--- src/main/java/lekcje/Rekurencja.java
+++ src/main/java/lekcje/Rekurencja.java
@@ -4,9 +4,10 @@ public class Rekurencja {
     public static int metoda(int n){
         if (n==0) return 1;
         if (n==1) return 2;
+        System.out.println(n);
         return metoda(n-1)+2*metoda(n-2);
     }
     public static void main(String[] args) {
-        System.out.println(metoda(4));
+        System.out.println(metoda(10));
     }
 }
