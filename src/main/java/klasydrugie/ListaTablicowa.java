0c32a0e531d27fa494f7c4be848289d9fc215d68
Tomasz Idzikowski <idzikdev@gmail.com>
2019-01-23 10:13:47 +0100

    poprawki

--- src/main/java/klasydrugie/ListaTablicowa.java
+++ src/main/java/klasydrugie/ListaTablicowa.java
@@ -4,19 +4,23 @@ import java.util.*;
 
 public class ListaTablicowa {
     public static void main(String[] args) {
-        List<Integer> lista=new ArrayList<>(Arrays.asList(4,5,13));
+        List<Integer> lista=new ArrayList<>(Arrays.asList(4,3,1,4));
         lista.add(10);
-        lista.add(9);
-        lista.add(14);
-        lista.remove(Integer.valueOf(14));
-        Collections.sort(lista);
-        Iterator<Integer> it= lista.iterator();
+        lista.add(1);
+        lista.add(2);
+        lista.add(10);
+        lista.add(0,12);
+        lista.remove(Integer.valueOf(10));
+        lista.remove(Integer.valueOf(10));
+        Collections.sort(lista,Collections.reverseOrder());
+        System.out.println(lista);
+        for (Integer el:lista
+             ) {
+            System.out.println(el);
+        }
+        Iterator<Integer> it=lista.iterator();
         while (it.hasNext()){
             System.out.println(it.next());
-        }
-        for (Integer el:lista
-        ) {
-            System.out.println(el);
         }
     }
 }
