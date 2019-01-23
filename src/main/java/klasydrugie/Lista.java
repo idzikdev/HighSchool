f9accc397be381f31f82e64df96255f84215bab2
Tomasz Idzikowski <idzikdev@gmail.com>
2019-01-23 08:44:43 +0100

    initial

---
+++ src/main/java/klasydrugie/Lista.java
@@ -0,0 +1,22 @@
+package klasydrugie;
+
+import java.util.*;
+
+public class Lista {
+    public static void main(String[] args) {
+        List<Integer> lista=new ArrayList<>(Arrays.asList(4,5,13));
+        lista.add(10);
+        lista.add(9);
+        lista.add(14);
+        lista.remove(Integer.valueOf(14));
+        Collections.sort(lista);
+        Iterator<Integer> it= lista.iterator();
+        while (it.hasNext()){
+            System.out.println(it.next());
+        }
+        for (Integer el:lista
+             ) {
+            System.out.println(el);
+        }
+    }
+}
