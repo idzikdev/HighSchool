63040f86bd5c656d829b68a6db0b7e7525ed9837
Tomasz Idzikowski <idzikdev@gmail.com>
2019-02-25 13:30:30 +0100

    new

--- src/main/java/poligon/funkcyjne/AppStudent.java
+++ src/main/java/poligon/funkcyjne/AppStudent.java
@@ -6,19 +6,27 @@ import java.util.List;
 import java.util.function.Predicate;
 
 public class AppStudent {
-    public static List<Student> met (List<Student> lista, Predicate<Student> predicate) {
+    public static void meet (List<Student> lista, Predicate<Student> predicate) {
 
         List<Student> result = new ArrayList<>();
 
         for (Student s : lista
         ) {
             if (predicate.test(s)){
-                result.add(s);
+                System.out.println(s);
             }
 
         }
-        return result;
+
     }
+
+    public static void show(List<Student>list, String name){
+        for (Student st:list
+             ) {
+            if (st.getName().equals(name)) System.out.println(st);
+        }
+    }
+
     public static void main(String[] args) {
         Student s1=Student.builder()
                 .name("Kasia")
@@ -35,11 +43,19 @@ public class AppStudent {
                 .surname("Tatara")
                 .age(23)
                 .build();
-        Predicate<Student> predicateKasia= student -> student.getName().equals("Kasia");
-        Predicate<Student> predicateDaniel= student -> student.getName().equals("Daniel");
-
         List<Student> list=new ArrayList<>(Arrays.asList(s1,s2,s3));
-        System.out.println(met(list,predicateKasia));
+
+        Predicate<Student> predicateKasia= student -> student.getName().equals("Kasia");
+        Predicate<Student> predicateWiek= student -> student.getAge()>30;
+
+        meet(list,predicateKasia.and(predicateWiek));
+
+
+
+//        Predicate<Student> predicateDaniel= student -> student.getName().equals("Daniel");
+//
+
+//        System.out.println(met(list,predicateKasia));
 
     }
 }
