6e12443c2c1565465e7a039a269ea6e86cd7db9d
Tomasz Idzikowski <idzikdev@gmail.com>
2019-06-04 11:17:30 +0200

    soldiers

---
+++ src/main/java/klasypierwsze/Soldier.java
@@ -0,0 +1,40 @@
+package klasypierwsze;
+
+import java.util.Random;
+
+public class Soldier {
+    private String name;
+    private String surname;
+    private String nationality;
+    private int age;
+    private int stamina;
+    private int damage;
+
+    public Soldier(String name, String surname, String nationality, int age, int stamina, int damage) {
+        this.name = name;
+        this.surname = surname;
+        this.nationality = nationality;
+        this.age = age;
+        this.stamina = stamina;
+        this.damage = damage;
+    }
+
+    @Override
+    public String toString() {
+        return "Soldier{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", nationality='" + nationality + '\'' +
+                ", age=" + age +
+                ", stamina=" + stamina +
+                ", damage=" + damage +
+                '}';
+    }
+
+    public void speak(){
+        String [] screams={"Go West","Forza Inter","Freeze"};
+        Random rnd=new Random();
+        int randomScream=rnd.nextInt(screams.length);
+        System.out.println(screams[randomScream]);
+    }
+}
---
+++ src/main/java/klasypierwsze/TestSoldier.java
@@ -0,0 +1,14 @@
+package klasypierwsze;
+
+public class TestSoldier {
+    public static void main(String[] args) {
+        Soldier soldier1=new Soldier("Jan",
+                "Kowalski","Polish",
+                31,90,80);
+
+        Soldier soldier2=new Soldier("Jacek","Placek","Polish"
+        ,30,87,90);
+
+
+    }
+}
